a44884853dfcf3fd3a853c63b42205a3cfd4315d
whitekiwi <jh145478@gmail.com>
2026-08-28 17:45:29 +0900

    style: finalize c6s formula order

--- Formula/c6s.rb
+++ Formula/c6s.rb
@@ -4,8 +4,8 @@ class C6s < Formula
   url "https://github.com/c6shq/homebrew-tap/releases/download/c6s-v0.1.1/c6s_v0.1.1_darwin_arm64.tar.gz"
   version "0.1.1"
   sha256 "9f987807855020e37a253a842e161f90b585aae5b5fbc47858c5fdc42980741a"
-  version_scheme 1
   license :cannot_represent
+  version_scheme 1
 
   depends_on arch: :arm64
   depends_on :macos
