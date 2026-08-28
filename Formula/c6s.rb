3d422a7b525082c67b1ae5bc04185dc08b697c98
whitekiwi <jh145478@gmail.com>
2026-08-28 17:45:04 +0900

    style: order c6s formula components

--- Formula/c6s.rb
+++ Formula/c6s.rb
@@ -3,8 +3,8 @@ class C6s < Formula
   homepage "https://c6s.whitekiwi.link"
   url "https://github.com/c6shq/homebrew-tap/releases/download/c6s-v0.1.1/c6s_v0.1.1_darwin_arm64.tar.gz"
   version "0.1.1"
-  version_scheme 1
   sha256 "9f987807855020e37a253a842e161f90b585aae5b5fbc47858c5fdc42980741a"
+  version_scheme 1
   license :cannot_represent
 
   depends_on arch: :arm64
