7c7d05c2a128eea25a70648af45b56dec5ec8eaa
whitekiwi <jh145478@gmail.com>
2026-09-01 00:28:33 +0900

    feat: publish c6s 0.5.13

--- Formula/c6s.rb
+++ Formula/c6s.rb
@@ -1,9 +1,9 @@
 class C6s < Formula
   desc "Agent-friendly client for c6s — Cerberus"
   homepage "https://c6s.whitekiwi.link"
-  url "https://github.com/c6shq/homebrew-tap/releases/download/c6s-v0.5.12/c6s_v0.5.12_darwin_arm64.tar.gz"
-  version "0.5.12"
-  sha256 "88bdd66db140363979f2bb5947a426825a1e68caabfc1775fda63ce7dced8cee"
+  url "https://github.com/c6shq/homebrew-tap/releases/download/c6s-v0.5.13/c6s_v0.5.13_darwin_arm64.tar.gz"
+  version "0.5.13"
+  sha256 "0f0c895631df314b4e92790f7230e5050dcb5c2eebab21e1450477d44f159405"
   license :cannot_represent
   version_scheme 1
 
